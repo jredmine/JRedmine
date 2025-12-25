4923db0e982b3857b2ac8d8314f009c58abc5f7d
panfeng <TangQingFengPHP@protonmail.com>
2025-12-25 22:33:46 +0800

    feat: 配置Swagger UI Bearer Token认证

--- src/main/java/com/github/jredmine/config/OpenApiConfig.java
+++ src/main/java/com/github/jredmine/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package com.github.jredmine.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +20,8 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("JRedmine API 文档")
@@ -27,7 +32,14 @@ public class OpenApiConfig {
                                 .email("support@jredmine.com"))
                         .license(new License()
                                 .name("Apache 2.0")
-                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("请输入JWT Token，格式：Bearer {token}")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName));
     }
 }
 
